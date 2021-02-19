445b64012e1a394542e63edc0377bae82f92881d
wasabeef <dadadada.chop@gmail.com>
2021-02-19 14:12:59 +0900

    chore(core): add build.yaml for pubspec.dart

--- packages/core/lib/settings/pubspec.dart
+++ packages/core/lib/settings/pubspec.dart
@@ -2,7 +2,7 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'pubspec.g.dart';
 
-/// Edit the file under _internal/lib/src/, then run `make generate-config-model`
+/// Edit this file, then run `make generate-config-model`
 
 @JsonSerializable()
 class Pubspec {
