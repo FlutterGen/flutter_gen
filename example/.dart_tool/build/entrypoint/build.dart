7d5de707c38a0441e552736255b74c472a854b53
Daichi Furiya <dadadada.chop@gmail.com>
2021-09-13 16:06:47 +0900

    chore: update dependencies (#125)
    * chore: update dependencies
    
    * chore: update android gradle wrapper

--- example/.dart_tool/build/entrypoint/build.dart
+++ example/.dart_tool/build/entrypoint/build.dart
@@ -1,10 +1,4 @@
-// Ensure that the build script itself is not opted in to null safety,
-// instead of taking the language version from the current package.
-//
-// @dart=2.9
-//
 // ignore_for_file: directives_ordering
-
 import 'package:build_runner_core/build_runner_core.dart' as _i1;
 import 'package:flutter_gen_runner/flutter_gen_runner.dart' as _i2;
 import 'dart:isolate' as _i3;
@@ -16,7 +10,7 @@ final _builders = <_i1.BuilderApplication>[
       _i1.toDependentsOf(r'flutter_gen_runner'),
       hideOutput: false)
 ];
-void main(List<String> args, [_i3.SendPort sendPort]) async {
+void main(List<String> args, [_i3.SendPort? sendPort]) async {
   var result = await _i4.run(args, _builders);
   sendPort?.send(result);
   _i5.exitCode = result;
