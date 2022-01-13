aca6c69f7fc67282b44ba8c223025db6b6624bed
Daichi Furiya <dadadada.chop@gmail.com>
2022-01-13 17:07:52 +0900

    chore: update dependencies (#189)
    * chore: update dependencies
    
    * chore: update dependencies

--- example/lib/freezed_sample.freezed.dart
+++ example/lib/freezed_sample.freezed.dart
@@ -1,5 +1,6 @@
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
 
 part of 'freezed_sample.dart';
@@ -155,11 +156,12 @@ class _$Data with DiagnosticableTreeMixin implements Data {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
             other is Data &&
-            (identical(other.value, value) || other.value == value));
+            const DeepCollectionEquality().equals(other.value, value));
   }
 
   @override
-  int get hashCode => Object.hash(runtimeType, value);
+  int get hashCode =>
+      Object.hash(runtimeType, const DeepCollectionEquality().hash(value));
 
   @JsonKey(ignore: true)
   @override
@@ -414,11 +416,12 @@ class _$ErrorDetails with DiagnosticableTreeMixin implements ErrorDetails {
     return identical(this, other) ||
         (other.runtimeType == runtimeType &&
             other is ErrorDetails &&
-            (identical(other.message, message) || other.message == message));
+            const DeepCollectionEquality().equals(other.message, message));
   }
 
   @override
-  int get hashCode => Object.hash(runtimeType, message);
+  int get hashCode =>
+      Object.hash(runtimeType, const DeepCollectionEquality().hash(message));
 
   @JsonKey(ignore: true)
   @override
