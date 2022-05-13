f15cbb0fe39ec75cc8980fe2b9ca5e458c4b6a8e
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2022-05-13 08:51:10 +0800

    Update generator_helper.dart

--- packages/core/lib/generators/generator_helper.dart
+++ packages/core/lib/generators/generator_helper.dart
@@ -8,7 +8,7 @@ String get header {
 }
 
 String get ignoreAnalysis {
-  return '''// ignore_for_file: directives_ordering,unnecessary_import
+  return '''// ignore_for_file: directives_ordering, unnecessary_import, use_super_parameters
   
 ''';
 }
