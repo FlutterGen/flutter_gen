3e6d3a9ac453a6873766329ed7ee259089c094a4
wasabeef <dadadada.chop@gmail.com>
2023-02-07 14:37:07 +0900

    chore: add prettier

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  bracketSpacing: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  plugins: ['prettier-plugin-packagejson'],
+};
