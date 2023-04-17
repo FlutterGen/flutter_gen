47368d89bd2e4b3ba63844e6a58d4557ac289d20
wasabeef <dadadada.chop@gmail.com>
2023-04-17 19:06:57 +0900

    update: re-generate svg codes

--- examples/example_resources/lib/gen/assets.gen.dart
+++ examples/example_resources/lib/gen/assets.gen.dart
@@ -141,9 +141,9 @@ class SvgGenImage {
     bool excludeFromSemantics = false,
     SvgTheme theme = const SvgTheme(),
     ColorFilter? colorFilter,
+    Clip clipBehavior = Clip.hardEdge,
     @deprecated Color? color,
     @deprecated BlendMode colorBlendMode = BlendMode.srcIn,
-    @deprecated Clip? clipBehavior,
     @deprecated bool cacheColorFilter = false,
   }) {
     return SvgPicture.asset(
