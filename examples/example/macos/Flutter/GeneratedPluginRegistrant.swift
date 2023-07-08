4e0764f7fa08da28767f80ec8618553d6dcade6f
Alex Li <github@alexv525.com>
2023-07-08 15:08:54 +0800

    ➖ Thin & upgrade dependencies

--- examples/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ examples/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,36 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import connectivity_plus
-import device_info_plus_macos
-import flutter_local_notifications
-import flutter_secure_storage_macos
-import geolocator_apple
-import in_app_review
-import package_info_plus_macos
-import path_provider_foundation
-import sentry_flutter
-import share_plus_macos
-import shared_preferences_macos
-import sign_in_with_apple
-import sqflite
-import url_launcher_macos
-import wakelock_macos
+import rive_common
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
-  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
-  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
-  FlutterSecureStorageMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageMacosPlugin"))
-  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
-  InAppReviewPlugin.register(with: registry.registrar(forPlugin: "InAppReviewPlugin"))
-  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
-  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
-  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
+  RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
 }
