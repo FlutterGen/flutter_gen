1609d71e58dc4d554e292595192b92d4fbcdd050
mym0404 <mym0404@gmail.com>
2024-03-18 04:12:55 +0900

    test: add test:flutter script & SvgTheme test in example

--- examples/example/lib/gen/assets.gen.dart
+++ examples/example/lib/gen/assets.gen.dart
@@ -165,8 +165,12 @@ class $AssetsImagesIconsGen {
   /// File path: assets/images/icons/paint.svg
   SvgGenImage get paint => const SvgGenImage('assets/images/icons/paint.svg');
 
+  /// File path: assets/images/icons/water@test.svg
+  SvgGenImage get waterTest =>
+      const SvgGenImage('assets/images/icons/water@test.svg');
+
   /// List of all assets
-  List<SvgGenImage> get values => [dartTest, fuchsia, kmm, paint];
+  List<SvgGenImage> get values => [dartTest, fuchsia, kmm, paint, waterTest];
 }
 
 class $AssetsLottieWrongGen {
