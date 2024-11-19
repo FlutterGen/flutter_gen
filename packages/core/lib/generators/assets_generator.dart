5f4a617cc15a64d8483ee54befe0cdb57da6dfbe
Ian Maciel <ian.maciel@motorolasolutions.com>
2024-11-19 14:03:46 -0300

    fixup! Add support for Flutter deferred components

--- packages/core/lib/generators/assets_generator.dart
+++ packages/core/lib/generators/assets_generator.dart
@@ -54,14 +54,11 @@ List<Object> _buildAssetsList(Config config) {
   // We may have several deferred components, with a list of assets for each.
   // So before spreading the list of deferred components, we need to spread
   // the list of assets for each deferred component.
-  final List<Object> deferredAssets = [];
-  config.pubspec.flutter.deferredComponents?.forEach((deferredComponent) {
-    // Include all manipulated assets to the list of deferred assets.
-    deferredAssets.addAll(deferredComponent.assets ?? []);
-  });
-
-  // Merge the deferred assets with the main assets.
-  return [...config.pubspec.flutter.assets, ...deferredAssets];
+  final deferredComponents = config.pubspec.flutter.deferredComponents ?? [];
+  return deferredComponents.fold<List<Object>>(
+    config.pubspec.flutter.assets,
+    (list, deferredComponent) => list + (deferredComponent.assets ?? []),
+  );
 }
 
 Future<String> generateAssets(
