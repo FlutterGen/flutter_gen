3514580a38b68a1fdf79a051737579c9a2d315b3
Alex Li <github@alexv525.com>
2024-12-16 14:18:14 +0800

    👷 Validate configurations

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -3,8 +3,11 @@
 set -o pipefail
 
 DIR="${1}"
-cd ${DIR}
+cd "${DIR}" || exit
+
+# Validate the configurations.
+curl --data-binary @codecov.yaml https://codecov.io/validate
+
 dart pub global activate coverage
 dart run coverage:test_with_coverage --port=9292
 format_coverage --lcov --in=coverage --out=coverage.lcov --report-on=lib
-
