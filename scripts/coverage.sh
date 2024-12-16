08127d1eb358a085ab50e5187335c0a12f56efd5
Alex Li <github@alexv525.com>
2024-12-16 14:36:29 +0800

    💚 Fix invalid codecov config (#622)

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+set -e
 set -o pipefail
 
+# Validate the configurations.
+curl --data-binary @codecov.yml https://codecov.io/validate
+
 DIR="${1}"
-cd ${DIR}
+cd "${DIR}" || exit
+
 dart pub global activate coverage
 dart run coverage:test_with_coverage --port=9292
 format_coverage --lcov --in=coverage --out=coverage.lcov --report-on=lib
-
