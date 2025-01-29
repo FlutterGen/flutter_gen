8e13d905ca4ff0fc0753f1900cb43995101fff16
Alex Li <github@alexv525.com>
2025-01-29 17:37:56 +0800

    ✅ Add tests

--- packages/core/test/settings_test.dart
+++ packages/core/test/settings_test.dart
@@ -1,6 +1,7 @@
 import 'package:collection/collection.dart';
 import 'package:flutter_gen_core/settings/asset_type.dart';
 import 'package:flutter_gen_core/settings/flavored_asset.dart';
+import 'package:flutter_gen_core/settings/pubspec.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -57,6 +58,33 @@ void main() {
           (e) => const SetEquality().equals(e.flavors, {'test'}),
         ),
       );
+    });
+  });
+
+  group(FlutterGenElementAssetsOutputsStyle, () {
+    test('fromJson', () {
+      expect(
+        FlutterGenElementAssetsOutputsStyle.fromJson('dot-delimiter'),
+        equals(FlutterGenElementAssetsOutputsStyle.dotDelimiterStyle),
+      );
+      expect(
+        FlutterGenElementAssetsOutputsStyle.fromJson('snake-case'),
+        equals(FlutterGenElementAssetsOutputsStyle.snakeCaseStyle),
+      );
+      expect(
+        FlutterGenElementAssetsOutputsStyle.fromJson('camel-case'),
+        equals(FlutterGenElementAssetsOutputsStyle.camelCaseStyle),
+      );
+      expect(
+        () => FlutterGenElementAssetsOutputsStyle.fromJson('wrong'),
+        throwsA(isA<ArgumentError>()),
+      );
+    });
+
+    test('toJson', () {
+      for (final style in FlutterGenElementAssetsOutputsStyle.values) {
+        expect(style.toJson(), equals(style.name));
+      }
     });
   });
 }
