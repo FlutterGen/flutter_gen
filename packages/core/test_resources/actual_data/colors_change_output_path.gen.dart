f0f3162d5ed5ea3bf1fbf4c85dff71a87dcfb64c
Alex Li <github@alexv525.com>
2025-04-07 09:48:14 +0800

    📸 Regenerate actual data

---
+++ packages/core/test_resources/actual_data/colors_change_output_path.gen.dart
@@ -0,0 +1,96 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  /// Color: #000000
+  static const Color black = Color(0xFF000000);
+
+  /// MaterialColor:
+  ///   50: #FFF9E5E5
+  ///   100: #FFF1BFBF
+  ///   200: #FFE79595
+  ///   300: #FFDD6A6A
+  ///   400: #FFD64A4A
+  ///   500: #FFCF2A2A
+  ///   600: #FFCA2525
+  ///   700: #FFC31F1F
+  ///   800: #FFBD1919
+  ///   900: #FFB20F0F
+  static const MaterialColor crimsonRed = MaterialColor(
+    0xFFCF2A2A,
+    <int, Color>{
+      50: Color(0xFFF9E5E5),
+      100: Color(0xFFF1BFBF),
+      200: Color(0xFFE79595),
+      300: Color(0xFFDD6A6A),
+      400: Color(0xFFD64A4A),
+      500: Color(0xFFCF2A2A),
+      600: Color(0xFFCA2525),
+      700: Color(0xFFC31F1F),
+      800: Color(0xFFBD1919),
+      900: Color(0xFFB20F0F),
+    },
+  );
+
+  /// Color: #979797
+  static const Color gray410 = Color(0xFF979797);
+
+  /// Color: #EEEEEE
+  static const Color gray70 = Color(0xFFEEEEEE);
+
+  /// Color: #FFFFFF
+  static const Color white = Color(0xFFFFFFFF);
+
+  /// MaterialColor:
+  ///   50: #FFFBF2E5
+  ///   100: #FFF5DFBE
+  ///   200: #FFEFCA93
+  ///   300: #FFE9B568
+  ///   400: #FFE4A547
+  ///   500: #FFDF9527
+  ///   600: #FFDB8D23
+  ///   700: #FFD7821D
+  ///   800: #FFD27817
+  ///   900: #FFCA670E
+  static const MaterialColor yellowOcher = MaterialColor(
+    0xFFDF9527,
+    <int, Color>{
+      50: Color(0xFFFBF2E5),
+      100: Color(0xFFF5DFBE),
+      200: Color(0xFFEFCA93),
+      300: Color(0xFFE9B568),
+      400: Color(0xFFE4A547),
+      500: Color(0xFFDF9527),
+      600: Color(0xFFDB8D23),
+      700: Color(0xFFD7821D),
+      800: Color(0xFFD27817),
+      900: Color(0xFFCA670E),
+    },
+  );
+
+  /// MaterialAccentColor:
+  ///   100: #FFFFE8E0
+  ///   200: #FFFFBCA3
+  ///   400: #FFFFA989
+  ///   700: #FFFF9E7A
+  static const MaterialAccentColor yellowOcherAccent = MaterialAccentColor(
+    0xFFFFBCA3,
+    <int, Color>{
+      100: Color(0xFFFFE8E0),
+      200: Color(0xFFFFBCA3),
+      400: Color(0xFFFFA989),
+      700: Color(0xFFFF9E7A),
+    },
+  );
+}
