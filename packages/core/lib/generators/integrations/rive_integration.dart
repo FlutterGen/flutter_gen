8573a44ae9c0123ef11c79615bd2a9c9ac7374b9
Alex Li <github@alexv525.com>
2026-03-04 11:42:00 +0800

    Improve integration constraints

--- packages/core/lib/generators/integrations/rive_integration.dart
+++ packages/core/lib/generators/integrations/rive_integration.dart
@@ -1,10 +1,9 @@
 import 'package:flutter_gen_core/generators/integrations/integration.dart';
-import 'package:pub_semver/pub_semver.dart' show Version, VersionConstraint;
-
-typedef RiveIntegrationLatest = RiveIntegration0140;
+import 'package:pub_semver/pub_semver.dart'
+    show Version, VersionConstraint, VersionRange;
 
 /// Create Rive integration based on the resolved version.
-abstract final class RiveIntegration extends Integration {
+sealed class RiveIntegration extends Integration {
   factory RiveIntegration(
     String packageName, {
     Version? resolvedVersion,
@@ -12,32 +11,39 @@ abstract final class RiveIntegration extends Integration {
   }) {
     // Resolve integration by version.
     RiveIntegration? integration = switch (resolvedVersion) {
-      final v? when v < Version(0, 14, 0) =>
+      final v? when v < RiveIntegration0140.minimumSupportVersion =>
         RiveIntegrationClassic(packageName),
-      Version() => RiveIntegrationLatest(packageName),
+      Version() => RiveIntegration0140(packageName),
       null => null,
     };
 
     // Resolve integration by version constraint.
     integration ??= switch (resolvedVersionConstraint) {
-      final c? when c.allows(Version(0, 14, 0)) =>
-        RiveIntegrationLatest(packageName),
+      final VersionRange c => switch (c.max) {
+          final max? when max == RiveIntegration0140.minimumSupportVersion =>
+            c.includeMax
+                ? RiveIntegration0140(packageName)
+                : RiveIntegrationClassic(packageName),
+          final max? when max > RiveIntegration0140.minimumSupportVersion =>
+            RiveIntegration0140(packageName),
+          _ => RiveIntegrationClassic(packageName),
+        },
       VersionConstraint() => RiveIntegrationClassic(packageName),
       null => null,
     };
 
     // Use the latest integration as the fallback.
-    integration ??= RiveIntegrationLatest(packageName);
+    integration ??= RiveIntegration0140(packageName);
 
     return integration;
   }
 
   RiveIntegration._(String packageName) : super(packageName);
-}
 
-/// Rive integration for versions before 0.14.0.
-final class RiveIntegrationClassic extends RiveIntegration {
-  RiveIntegrationClassic(String packageName) : super._(packageName);
+  String get _classDefinition;
+
+  @override
+  String get classOutput => _classDefinition;
 
   String? get packageExpression => isPackage ? 'packages/$packageName/' : null;
 
@@ -48,8 +54,20 @@ final class RiveIntegrationClassic extends RiveIntegration {
       ];
 
   @override
-  String get classOutput => _classDefinition;
+  String get className => 'RiveGenImage';
 
+  @override
+  bool isSupport(AssetType asset) => asset.extension == '.riv';
+
+  @override
+  bool get isConstConstructor => true;
+}
+
+/// Rive integration for versions before 0.14.0.
+final class RiveIntegrationClassic extends RiveIntegration {
+  RiveIntegrationClassic(String packageName) : super._(packageName);
+
+  @override
   String get _classDefinition => '''class RiveGenImage {
   const RiveGenImage(
     this._assetName, {
@@ -92,20 +110,13 @@ ${isPackage ? "\n  static const String package = '$packageName';" : ''}
 
   String get keyName => ${isPackage ? '\'$packageExpression\$_assetName\'' : '_assetName'};
 }''';
-
-  @override
-  String get className => 'RiveGenImage';
-
-  @override
-  bool isSupport(AssetType asset) => asset.extension == '.riv';
-
-  @override
-  bool get isConstConstructor => true;
 }
 
 /// Rive integration for versions equal to or above 0.14.0.
-final class RiveIntegration0140 extends RiveIntegrationClassic {
-  RiveIntegration0140(String packageName) : super(packageName);
+final class RiveIntegration0140 extends RiveIntegration {
+  RiveIntegration0140(String packageName) : super._(packageName);
+
+  static final minimumSupportVersion = Version(0, 14, 0, pre: '0');
 
   @override
   String get _classDefinition => '''class RiveGenImage {
